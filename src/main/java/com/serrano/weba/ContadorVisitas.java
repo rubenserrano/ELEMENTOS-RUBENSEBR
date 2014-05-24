56a8325bdc275e7219fa7cefe3583c066e85632f
T107 <T107@PC246>
2014-05-23 21:34:59 -0500

    Mi practica de hoy

---
+++ src/main/java/com/serrano/weba/ContadorVisitas.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.serrano.weba;
+
+/**
+ *
+ * @author T107
+ */
+public class ContadorVisitas {
+         private static int conta;
+    
+
+    public static int getConta() {
+        conta++;
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+    
+   
+  
+    
+    
+   
+    
+
+       
+    
+}
